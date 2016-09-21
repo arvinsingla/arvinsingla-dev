e9f6ce67fd6173d8dfcd7ca3557db1e8a2e02ca8
Arvin Singla <arvin.singla@acquia.com>
2016-09-21 01:11:28 -0400

    Updated resume content and added print stylesheet.

--- config/environment.js
+++ config/environment.js
@@ -4,7 +4,7 @@ module.exports = function(environment) {
   var ENV = {
     modulePrefix: 'arvinsingla',
     environment: environment,
-    baseURL: '/',
+    rootURL: '/',
     locationType: 'auto',
     EmberENV: {
       FEATURES: {
@@ -47,7 +47,7 @@ module.exports = function(environment) {
 
   if (environment === 'test') {
     // Testem prefers this...
-    ENV.baseURL = '/';
+    ENV.rootURL = '/';
     ENV.locationType = 'none';
 
     // keep test console output quieter
